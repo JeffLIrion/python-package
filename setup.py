af3f7d55a88f3614a00cdd38ce60da35f447a6c9
Jeff Irion <JeffLIrion@users.noreply.github.com>
2020-02-04 06:53:26 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup
 
+with open('README.rst') as f:
+    readme = f.read()
+
 setup(
     name='python_package',
     version='0.0.1',
     description='Python package template',
+    long_description=readme,
+    # keywords=[],
+    # url='https://github.com/JeffLIrion/python-package',
+    license='MIT',
     author='Jeff Irion',
     author_email='jefflirion@users.noreply.github.com',
     packages=['python_package'],
     # install_requires=[],
-    classifiers=['Operating System :: OS Independent',
+    # tests_require=[],
+    classifiers=['License :: OSI Approved :: MIT License', 
+                 'Operating System :: OS Independent',
                  'Programming Language :: Python :: 3'],
     test_suite='tests'
 )
