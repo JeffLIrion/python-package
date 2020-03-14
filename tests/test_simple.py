862d13fff652f508fb6550815ec61eb33a762386
Jeff Irion <jefflirion@users.noreply.github.com>
2020-03-14 13:23:42 -0700

    Fix linting for the tests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -3,22 +3,24 @@
 """
 
 
-import sys
 import unittest
-
-sys.path.insert(0, '..')
 
 from python_package import simple
 
 
 class TestSimple(unittest.TestCase):
+    """Simple tests."""
+
     def setUp(self):
+        """Setup for the tests."""
         self.s = simple.create_simple()
 
     def test_get_data(self):
+        """Test the ``get_data()`` method."""
         self.assertEqual(self.s.get_data(), 5)
 
     def test_set_data(self):
+        """Test the ``set_data()`` method."""
         self.s.set_data(6)
         self.assertEqual(self.s.get_data(), 6)
 
